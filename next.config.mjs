4e86e5b5700a2824721a8d769d6645336f1aba4e
Thomas Guillet <thomas@palya.fr>
2024-06-17 23:16:19 +0200

    Corrige la config et ajoute prettier

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,13 @@
 /** @type {import('next').NextConfig} */
 
-const isProduction = 'production' === process.env.NODE_ENV
+const isProduction = "production" === process.env.NODE_ENV;
 
 const nextConfig = {
   reactStrictMode: true,
-  output: 'export',
+  output: "export",
   assetPrefix: isProduction ? "/anti-comparateur" : "",
   basePath: isProduction ? "/anti-comparateur" : "",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
