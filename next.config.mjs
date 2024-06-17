14b1bbe3dee298af29103a2143a1a2f70d74736c
Thomas Guillet <thomas@palya.fr>
2024-06-17 23:11:00 +0200

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const isProduction = 'production' === process.env.NODE_ENV
+
 const nextConfig = {
   reactStrictMode: true,
+  output: 'export',
+  assetPrefix: isProduction ? "/anti-comparateur" : "",
+  basePath: isProduction ? "/anti-comparateur" : "",
 };
 
 export default nextConfig;
