0396b58d39be9021a657e255c6bdf39ad3b5136a
Thomas Guillet <thomas@palya.fr>
2024-06-20 22:03:19 +0200

    Déploie sans sous chemin

--- next.config.mjs
+++ next.config.mjs
@@ -5,8 +5,6 @@ const isProduction = "production" === process.env.NODE_ENV;
 const nextConfig = {
   reactStrictMode: true,
   output: isProduction ? "export" : undefined,
-  assetPrefix: isProduction ? "/anti-comparateur" : "",
-  basePath: isProduction ? "/anti-comparateur" : "",
   images: { unoptimized: true },
 };
 
