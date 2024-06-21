96e3b05ba71c74734dc3bf543a1a7a8399c6f291
Thomas Guillet <thomas@palya.fr>
2024-06-21 17:46:48 +0200

    Ajoute du déploiement périodique

---
+++ periodic_build.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+./import.sh
+RESULT=`node --eval "const dates = require('./data/dates.json'); console.log(dates.records[0].fields.TODO);"`
+if "$RESULT" == "false";
+then
+	echo "Nothing new, no need to build"
+	exit 0
+fi
+
+curl -X PATCH https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Dates/records -H "Content-Type: application/json" -d @- <<EOF
+{
+	"records": [{
+		"id": 1,
+		"fields": {
+			"Manuel": "`date --iso-8601=seconds`"
+		}
+	}]
+}
+EOF
+
+npm ci
+npm run build
+touch out/.nojekyll
+
+echo "deployment_required=true" >> $GITHUB_OUTPUT
