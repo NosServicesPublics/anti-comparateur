faafc0627ab3e10fdff846f8637991370d1218ff
Thomas Guillet <thomas@palya.fr>
2024-06-25 01:38:55 +0200

    Ajoute le double tri aux questions

--- import.sh
+++ import.sh
@@ -3,6 +3,6 @@
 set -ve
 
 curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Thematiques/records?sort=Position" > data/thematiques.json
-curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Questions/records?sort=Position" > data/questions.json
+curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Questions/records?sort=Thematique_position,Position" > data/questions.json
 curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Reponses/records?sort=Qui" > data/reponses.json
 curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Dates/records" > data/dates.json
