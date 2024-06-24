5c2d8c44c0f12467001ebc539bda0643a768532c
Thomas Guillet <thomas@palya.fr>
2024-06-24 19:01:20 +0200

    Ajoute Grist pour la prévisualisation

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,7 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import Script from 'next/script';
 
 export default function Document() {
   return (
@@ -11,6 +12,11 @@ export default function Document() {
         <Main />
         <NextScript />
         <Footer />
+        <Script
+          src="https://grist.incubateur.net/grist-plugin-api.js"
+          strategy="beforeInteractive"
+          async=""
+        />
       </body>
     </Html>
   );
