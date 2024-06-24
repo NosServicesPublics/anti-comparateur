5324cec867d53161e513c29b2127342b71c69d94
Thomas Guillet <thomas@palya.fr>
2024-06-25 01:27:16 +0200

    Modifie la logique d'ordonnancement des thématiques et des questions

--- import.sh
+++ import.sh
@@ -2,7 +2,7 @@
 
 set -ve
 
-curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Thematiques/records?sort=Nom" > data/thematiques.json
-curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Questions/records?sort=Intitule" > data/questions.json
+curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Thematiques/records?sort=Position" > data/thematiques.json
+curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Questions/records?sort=Position" > data/questions.json
 curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Reponses/records?sort=Qui" > data/reponses.json
 curl "https://nosservicespublics.getgrist.com/api/docs/hXBbY6ngk5ik/tables/Dates/records" > data/dates.json
