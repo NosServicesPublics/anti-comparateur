3ed3f0cacf83b39de025d0edd532ee316bff98dd
Thomas Guillet <thomas@palya.fr>
2024-06-24 19:05:30 +0200

    Supprime une page déjà légacy et ajoute la page de preview

---
+++ pages/preview.js
@@ -0,0 +1,50 @@
+import { NextSeo } from 'next-seo';
+import Link from "next/link";
+import Script from 'next/script';
+
+import { useState, useEffect } from "react";
+import ResponsePageContent from "@/components/ResponsePageContent";
+
+import {
+  getThematiqueKey,
+} from "@/lib/data-mappings";
+
+export default function PreviewPage() {
+  const [thematique, setThematique] = useState();
+  const [question, setQuestion] = useState();
+  const [reponse, setReponse] = useState();
+  const [logs, setLogs] = useState([])
+
+  useEffect(() => {
+    window.grist.ready()
+    window.grist.onRecord((record) => {
+      if (!record) {
+        return
+      }
+      setReponse({id: record.id, fields: record})
+      setThematique({fields: {
+        Slug: record.Thematique_Slug,
+        Nom: record.Thematique
+      }})
+      setQuestion({fields: {
+        Intitule: record.Question
+      }})
+    })
+
+
+  }, [])
+
+  const thematiqueKey = getThematiqueKey(thematique);
+  return (thematique && question && reponse) && (
+    <>
+      <NextSeo
+        title={'Réponse'}
+      />
+      <ResponsePageContent
+          thematique={thematique}
+          question={question}
+          reponse={reponse}
+        />
+    </>
+  );
+}
